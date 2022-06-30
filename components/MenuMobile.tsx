54882549cfd33afac2f44b1f1af98f2d6368411b
cristianocsl <cslcristiano@gmail.com>
2022-06-30 18:57:28 -0300

    styles: adiciona estilização do header para mobile

--- components/MenuMobile.tsx
+++ components/MenuMobile.tsx
@@ -20,10 +20,11 @@ import {
   useDisclosure,
   Button,
   Input,
+  Image,
 } from '@chakra-ui/react'
 import { HamburgerIcon, SearchIcon } from '@chakra-ui/icons'
 import styles from '../styles/Home.module.css'
-
+import styled from 'styled-components'
 
 export default function MenuMobile() {
   const { isOpen, onOpen, onClose } = useDisclosure()
@@ -31,51 +32,44 @@ export default function MenuMobile() {
 
   return (
     <div className={styles.mobile}>
-      <Flex alignItems='center' justifyContent='space-around' justifyItems={'center'}>
-        <>
-          <Button ref={btnRef} colorScheme='teal' onClick={onOpen}>
-            <HamburgerIcon />
-          </Button>
-          <Box mr={40}>WINE</Box>
-        </>
+      <Flex alignItems='center' ml={'-1em'} align={'center'}>
+        <Button ref={btnRef} colorScheme='teal' mr={'27px'} onClick={onOpen}>
+          <HamburgerIcon boxSize={'27px'}/>
+        </Button>
+        <Image mr={100} src={'white.svg'} height={'22px'}/>
 
-        <>
-          <Drawer
-            isOpen={isOpen}
-            placement='left'
-            onClose={onClose}
-            finalFocusRef={btnRef}
-          >
-            <DrawerOverlay />
-            <DrawerContent>
-              <DrawerCloseButton />
-              <DrawerHeader>Create your account</DrawerHeader>
+        <Drawer
+          isOpen={isOpen}
+          placement='left'
+          onClose={onClose}
+          finalFocusRef={btnRef}
+        >
+          <DrawerOverlay />
+          <DrawerContent>
+            <DrawerCloseButton />
+            <DrawerHeader>Create your account</DrawerHeader>
 
-              <DrawerBody>
-                <Input placeholder='Type here...' />
-              </DrawerBody>
+            <DrawerBody>
+              <Input placeholder='Type here...' />
+            </DrawerBody>
 
-              <DrawerFooter>
-                <Button variant='outline' mr={3} onClick={onClose}>
-                  Cancel
-                </Button>
-                <Button colorScheme='blue'>Save</Button>
-              </DrawerFooter>
-            </DrawerContent>
-          </Drawer>
-          <AvatarGroup spacing={20}>
-            <Circle size='40px' borderWidth={1} borderColor={'gray'}>
-              <SearchIcon />
-            </Circle>
-            <Circle size='40px' borderWidth={1} borderColor={'gray'}>
-              <Avatar boxSize={40}>
-                <AvatarBadge borderColor='black' boxSize='1.5em' bg='red' borderRadius={'100%'} >
-                  0
-                </AvatarBadge>
-              </Avatar>
-            </Circle>
-          </AvatarGroup>
-        </>
+            <DrawerFooter>
+              <Button variant='outline' mr={3} onClick={onClose}>
+                Cancel
+              </Button>
+              <Button colorScheme='blue'>Save</Button>
+            </DrawerFooter>
+          </DrawerContent>
+        </Drawer>
+        <AvatarGroup spacing={20}>
+          <SearchIcon boxSize='26px' color={'gray'}/>
+          <Circle bg={'#F6B554'} size='56px'>
+            <Image boxSize={'55px'} src={'Bitmap.svg'} borderBottomRadius={'100%'} position={'absolute'}/>
+              <Circle position={'absolute'} borderColor='black' mt={'32px'} ml={'32px'} size='1.5em' bg='white' borderRadius={'100%'} >
+                0
+              </Circle>
+          </Circle>
+        </AvatarGroup>
       </Flex>
     </div>
   )
