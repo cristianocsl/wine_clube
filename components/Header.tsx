b27708a55cdaaac0f8bb3d33a4d125a998301cd9
cristianocsl <cslcristiano@gmail.com>
2022-06-30 15:44:24 -0300

    feat: recebe componentes de  menu para desktop e mobile a partir do css

--- components/Header.tsx
+++ components/Header.tsx
@@ -11,50 +11,16 @@ import {
   AvatarGroup,
   AvatarBadge,
 } from '@chakra-ui/react'
-import { SearchIcon } from '@chakra-ui/icons'
+import { SearchIcon, HamburgerIcon } from '@chakra-ui/icons'
+import MenuDesktop from "./MenuDesktop";
+import MenuMobile from "./MenuMobile";
+import styles from '../styles/Home.module.css';
 
 export default function Header() {
   return (
-    <Flex>
-      <Box mr={40}>WINE</Box>
-      <Breadcrumb separator='' mr={40}>
-        <BreadcrumbItem>
-          <BreadcrumbLink href='#'>Clube</BreadcrumbLink>
-        </BreadcrumbItem>
-
-        <BreadcrumbItem>
-          <BreadcrumbLink isCurrentPage href='#'>Loja</BreadcrumbLink>
-        </BreadcrumbItem>
-
-        <BreadcrumbItem>
-          <BreadcrumbLink href='#'>Produtores</BreadcrumbLink>
-        </BreadcrumbItem>
-
-        <BreadcrumbItem>
-          <BreadcrumbLink href='#'>Produtores</BreadcrumbLink>
-        </BreadcrumbItem>
-
-        <BreadcrumbItem>
-          <BreadcrumbLink href='#'>Ofertas</BreadcrumbLink>
-        </BreadcrumbItem>
-
-        <BreadcrumbItem>
-          <BreadcrumbLink href='#'>Eventos</BreadcrumbLink>
-        </BreadcrumbItem>
-      </Breadcrumb>
-      <AvatarGroup spacing={20}>
-        <Circle size='40px' borderWidth={1} borderColor={'gray'}>
-          <SearchIcon />
-        </Circle>
-        <Circle size='40px' borderWidth={1} borderColor={'gray'}>
-          <Avatar boxSize={40} color='red'/>
-        </Circle>
-        <Circle size='40px' borderWidth={1} borderColor={'gray'}>
-          <Avatar boxSize={40}>
-            <AvatarBadge boxSize={40} bg='green' />
-          </Avatar>
-        </Circle>
-      </AvatarGroup>
+    <Flex mt={10} alignItems='center'>
+      <MenuMobile />
+      <MenuDesktop />
     </Flex>
   )
 }
