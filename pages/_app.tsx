e62857e2a82eb48ebc4ac0249c0a79097c3001d2
cristianocsl <cslcristiano@gmail.com>
2022-06-30 11:50:04 -0300

    ci: instalando chakra ui e styled-components

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,12 +3,15 @@ import type { AppProps } from 'next/app'
 import { ThemeProvider } from 'styled-components'
 import theme from '../styles/theme'
 import GlobalStyle from '../styles/global'
+import { ChakraProvider } from '@chakra-ui/react'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <Component {...pageProps} />
+      <ChakraProvider theme={theme}>
+        <GlobalStyle />
+        <Component {...pageProps} />
+      </ChakraProvider>
     </ThemeProvider>
   )
 }
