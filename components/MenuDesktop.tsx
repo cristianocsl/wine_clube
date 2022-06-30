78628ba5814acbdd53b7e341c46aaf252c1721a2
cristianocsl <cslcristiano@gmail.com>
2022-06-30 19:59:32 -0300

    styles: adiciona estilização do header para desktop

--- components/MenuDesktop.tsx
+++ components/MenuDesktop.tsx
@@ -5,6 +5,7 @@ import {
   BreadcrumbLink,
   BreadcrumbSeparator,
   Flex,
+  Image,
   Box,
   Circle,
   Avatar,
@@ -17,48 +18,48 @@ import styles from '../styles/Home.module.css'
 export default function MenuDesktop () {
   return (
     <div className={styles.desktop}>
-      <Box mr={40}>WINE</Box>
+      <Flex align={'center'}>
+        <Image ml={10} mr={40} src={'white.svg'} height={27}/>
 
-      <Breadcrumb separator='' mr={40}>
-        <BreadcrumbItem>
-          <BreadcrumbLink href='#'>Clube</BreadcrumbLink>
-        </BreadcrumbItem>
+        <Breadcrumb separator='' mr={70}>
+          <BreadcrumbItem>
+            <BreadcrumbLink href='#' fontSize={'12px'}>Clube</BreadcrumbLink>
+          </BreadcrumbItem>
 
-        <BreadcrumbItem>
-          <BreadcrumbLink isCurrentPage href='#'>Loja</BreadcrumbLink>
-        </BreadcrumbItem>
+          <BreadcrumbItem>
+            <BreadcrumbLink isCurrentPage href='#' fontSize={'12px'}>Loja</BreadcrumbLink>
+          </BreadcrumbItem>
 
-        <BreadcrumbItem>
-          <BreadcrumbLink href='#'>Produtores</BreadcrumbLink>
-        </BreadcrumbItem>
+          <BreadcrumbItem>
+            <BreadcrumbLink href='#' fontSize={'12px'}>Produtores</BreadcrumbLink>
+          </BreadcrumbItem>
 
-        <BreadcrumbItem>
-          <BreadcrumbLink href='#'>Produtores</BreadcrumbLink>
-        </BreadcrumbItem>
+          <BreadcrumbItem>
+            <BreadcrumbLink href='#' fontSize={'12px'}>Ofertas</BreadcrumbLink>
+          </BreadcrumbItem>
 
-        <BreadcrumbItem>
-          <BreadcrumbLink href='#'>Ofertas</BreadcrumbLink>
-        </BreadcrumbItem>
+          <BreadcrumbItem>
+            <BreadcrumbLink href='#' fontSize={'12px'}>Eventos</BreadcrumbLink>
+          </BreadcrumbItem>
+        </Breadcrumb>
 
-        <BreadcrumbItem>
-          <BreadcrumbLink href='#'>Eventos</BreadcrumbLink>
-        </BreadcrumbItem>
-      </Breadcrumb>
-      <AvatarGroup spacing={20}>
-        <Circle size='40px' borderWidth={1} borderColor={'gray'}>
-          <SearchIcon />
-        </Circle>
-        <Circle size='40px' borderWidth={1} borderColor={'gray'}>
-          <Avatar boxSize={40} color='red'/>
-        </Circle>
-        <Circle size='40px' borderWidth={1} borderColor={'gray'}>
-          <Avatar boxSize={40}>
-            <AvatarBadge borderColor='black' boxSize='1.5em' bg='red' borderRadius={'100%'} >
+        <AvatarGroup spacing={20}>
+          <Circle size='44px' borderWidth={1} borderColor={'gray'}>
+            <SearchIcon />
+          </Circle>
+
+          <Circle size='44px' borderWidth={1} borderColor={'gray'}>
+            <Avatar boxSize={45} color='red'/>
+          </Circle>
+
+          <Circle bg={'#F6B554'} size='44px'>
+            <Image boxSize={'45px'} src={'Bitmap.svg'} borderBottomRadius={'100%'} position={'absolute'}/>
+            <Circle position={'absolute'} borderColor='black' mt={'32px'} ml={'32px'} size='1.5em' bg='white' borderRadius={'100%'} fontSize={'12px'}>
               0
-            </AvatarBadge>
-          </Avatar>
-        </Circle>
-      </AvatarGroup>
+            </Circle>
+          </Circle>
+        </AvatarGroup>
+      </Flex>
     </div>
   );
 }
