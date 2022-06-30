028448e0ce10b0f158d5eedadbc90a3509b34f53
cristianocsl <cslcristiano@gmail.com>
2022-06-29 21:58:15 -0300

    ci: configurações iniciais

---
+++ styles/theme.ts
@@ -0,0 +1,63 @@
+const theme = {
+  colors: {
+    primaryColor: '#0A1128',
+    secondaryColor: '#dc143c',
+    white: '#FFFFFF',
+    mediumGray: '#DDDDDD',
+    background: '#F5F8FA',
+    text: 'black',
+    brand: {
+      blue: '#115D8C',
+      orange: '#F25D27',
+    },
+    textColors: {
+      titles: '#123952',
+      text: '#617480',
+      complement: '#A0ACB3',
+      gray5: '#555555',
+      gray6: '#666666',
+      gray8: '#888888',
+      pinkStore: '#D14B8F',
+      pinkPrice1: '#C81A78',
+      pinkPrece2: '#B6116E',
+      titleFilter: '#000000',
+      titlePrice: '#262626',
+      found: '#262626',
+      radioButtom: '#1D1D1B',
+    },
+    radioButtom: '#EAEAEA',
+    green: '##7EBC43',
+    discount: '#F79552',
+  },
+  font: {
+    family: {
+      default: "'Open Sans', sans-serif",
+      secondary: "'Montserrat', sans-serif",
+    },
+    sizes: {
+      xsmall: '8rem',
+      small: '1.6rem',
+      medium: '2.4rem',
+      large: '3.2rem',
+      xlarge: '4.0rem',
+      xxlarge: '4.8rem',
+      huge: '5.6rem',
+      xhuge: '6.4rem',
+    },
+  },
+  media: {
+    lteMedium: '(max-width: 768px)',
+  },
+  spacings: {
+    xsmall: '8rem',
+    small: '1.6rem',
+    medium: '2.4rem',
+    large: '3.2rem',
+    xlarge: '4.0rem',
+    xxlarge: '4.8rem',
+    huge: '5.6rem',
+    xhuge: '6.4rem',
+  },
+}
+
+export default theme
