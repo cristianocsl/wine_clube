f99ffc192bf4fe063cc824159aa256324b88d589
cristianocsl <cslcristiano@gmail.com>
2022-07-01 18:48:19 -0300

    styles: adiciona ajustes de estilização para versão mobile

--- components/MenuMobile.tsx
+++ components/MenuMobile.tsx
@@ -24,6 +24,7 @@ import {
 } from '@chakra-ui/react'
 import { HamburgerIcon, SearchIcon } from '@chakra-ui/icons'
 import styles from '../styles/Home.module.css'
+import MenuMobileContent from './MenuMobileContent'
 
 export default function MenuMobile() {
   const { isOpen, onOpen, onClose } = useDisclosure()
@@ -31,7 +32,7 @@ export default function MenuMobile() {
 
   return (
     <div className={styles.mobile}>
-      <Flex height={'72px'} align={'center'} bg={'red'} width={'100%'} mt={-10} pr={'16px'} justifyContent={'space-between'}>
+      <Flex height={'72px'} align={'center'} bg={'white'} width={'100%'} mt={-10} pr={'16px'} justifyContent={'space-between'}>
 
         <Flex align={'center'} ml={'18.75px'}>
           <Button ref={btnRef} colorScheme='teal' mr={'27px'} onClick={onOpen}>
@@ -41,29 +42,31 @@ export default function MenuMobile() {
           <Image mr={100} src={'white.svg'} height={'19px'}/>
         </Flex>
 
-        <Drawer
-          isOpen={isOpen}
-          placement='left'
-          onClose={onClose}
-          finalFocusRef={btnRef}
-        >
-          <DrawerOverlay />
-          <DrawerContent>
-            <DrawerCloseButton />
-            <DrawerHeader>Create your account</DrawerHeader>
+        <>
+          <Drawer
+            isOpen={isOpen}
+            placement='left'
+            onClose={onClose}
+            finalFocusRef={btnRef}
+          >
+            <DrawerOverlay bg={'#F5F8FA'} width={'280px'}/>
+            <DrawerContent alignItems={'start'}>
+              <DrawerCloseButton alignSelf={'start'} mt={'20px'} ml={'245px'} color={'black'}/>
 
-            <DrawerBody>
-              <Input placeholder='Type here...' />
-            </DrawerBody>
+              <DrawerHeader display={'flex'} bg={'#F5F8FA'} alignItems={'center'} position={'relative'} mt={'-10px'} ml={'20px'}>
+                <Avatar boxSize={'56px'} bg={'gray'} borderRadius={'100%'} mr={'20px'} />
+                <Box>
+                  <Box color={'gray'}>Acesse sua conta</Box>
+                  <Box color={'#D14B8F'} pt={'10px'}>Entrar</Box>
+                </Box>
+              </DrawerHeader>
 
-            <DrawerFooter>
-              <Button variant='outline' mr={3} onClick={onClose}>
-                Cancel
-              </Button>
-              <Button colorScheme='blue'>Save</Button>
-            </DrawerFooter>
-          </DrawerContent>
-        </Drawer>
+              <DrawerBody>
+                <MenuMobileContent/>
+              </DrawerBody>
+            </DrawerContent>
+          </Drawer>
+        </>
         <AvatarGroup spacing={25}>
           <SearchIcon boxSize='26px' color={'gray'}/>
           <Circle bg={'#F6B554'} size='56px'>
