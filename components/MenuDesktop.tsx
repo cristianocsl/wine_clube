b53ac6513f5c82953fffcb2ea76084774202123f
cristianocsl <cslcristiano@gmail.com>
2022-07-01 00:11:13 -0300

    styles: ajustes na estilização responsiva para mobile/desktop

--- components/MenuDesktop.tsx
+++ components/MenuDesktop.tsx
@@ -18,43 +18,53 @@ import styles from '../styles/Home.module.css'
 export default function MenuDesktop () {
   return (
     <div className={styles.desktop}>
-      <Flex align={'center'}>
-        <Image ml={10} mr={40} src={'white.svg'} height={27}/>
+      <Flex align={'center'} height={'80px'} bg={'red'} width={'100%'} mt={-10} justifyContent={'space-around'}>
+        <Flex align={'center'}>
+          <Image ml={10} mr={40} src={'white.svg'} height={27.77}/>
 
-        <Breadcrumb separator='' mr={70}>
-          <BreadcrumbItem>
-            <BreadcrumbLink href='#' fontSize={'12px'}>Clube</BreadcrumbLink>
-          </BreadcrumbItem>
+          <Breadcrumb separator='' mr={70}>
+            <BreadcrumbItem>
+              <BreadcrumbLink href='#' fontSize={'20px'}>Clube</BreadcrumbLink>
+            </BreadcrumbItem>
 
-          <BreadcrumbItem>
-            <BreadcrumbLink isCurrentPage href='#' fontSize={'12px'}>Loja</BreadcrumbLink>
-          </BreadcrumbItem>
+            <BreadcrumbItem>
+              <BreadcrumbLink isCurrentPage href='#' fontSize={'20px'}>Loja</BreadcrumbLink>
+            </BreadcrumbItem>
 
-          <BreadcrumbItem>
-            <BreadcrumbLink href='#' fontSize={'12px'}>Produtores</BreadcrumbLink>
-          </BreadcrumbItem>
+            <BreadcrumbItem>
+              <BreadcrumbLink href='#' fontSize={'20px'}>Produtores</BreadcrumbLink>
+            </BreadcrumbItem>
 
-          <BreadcrumbItem>
-            <BreadcrumbLink href='#' fontSize={'12px'}>Ofertas</BreadcrumbLink>
-          </BreadcrumbItem>
+            <BreadcrumbItem>
+              <BreadcrumbLink href='#' fontSize={'20px'}>Ofertas</BreadcrumbLink>
+            </BreadcrumbItem>
 
-          <BreadcrumbItem>
-            <BreadcrumbLink href='#' fontSize={'12px'}>Eventos</BreadcrumbLink>
-          </BreadcrumbItem>
-        </Breadcrumb>
+            <BreadcrumbItem>
+              <BreadcrumbLink href='#' fontSize={'20px'}>Eventos</BreadcrumbLink>
+            </BreadcrumbItem>
+          </Breadcrumb>
+        </Flex>
 
         <AvatarGroup spacing={20}>
-          <Circle size='44px' borderWidth={1} borderColor={'gray'}>
+          <Circle size='56px' borderWidth={1} borderColor={'gray'}>
             <SearchIcon />
           </Circle>
 
-          <Circle size='44px' borderWidth={1} borderColor={'gray'}>
-            <Avatar boxSize={45} color='red'/>
+          <Circle size='56px' borderWidth={1} borderColor={'gray'}>
+            <Avatar boxSize={'64px'} color='red'/>
           </Circle>
 
-          <Circle bg={'#F6B554'} size='44px'>
-            <Image boxSize={'45px'} src={'Bitmap.svg'} borderBottomRadius={'100%'} position={'absolute'}/>
-            <Circle position={'absolute'} borderColor='black' mt={'32px'} ml={'32px'} size='1.5em' bg='white' borderRadius={'100%'} fontSize={'12px'}>
+          <Circle bg={'#F6B554'} size='56px'>
+            <Image boxSize={'55px'} src={'Bitmap.svg'} borderBottomRadius={'100%'} position={'absolute'}/>
+            <Circle
+              position={'absolute'}
+              borderColor='black'
+              mt={'36px'}
+              ml={'36px'}
+              size='2em'
+              bg='white'
+              borderRadius={'100%'}
+              fontSize={'12px'}>
               0
             </Circle>
           </Circle>
