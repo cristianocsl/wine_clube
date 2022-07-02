856d2b5eed8db9b69f0460d386b3d835f411dd3f
cristianocsl <cslcristiano@gmail.com>
2022-07-02 13:05:48 -0300

    styles: define esquema de sombreamento de cards

--- components/CardWine.tsx
+++ components/CardWine.tsx
@@ -1,21 +1,21 @@
-import { Center, WrapItem, Image, Box } from "@chakra-ui/react";
+import { Center, WrapItem, Image, Box, Text } from "@chakra-ui/react";
 import React from "react";
 import { WinesType } from "../services/typing";
 
 export default function CardWine(props: { wine: WinesType}) {
   const { wine } = props;
+  
   return (
-    <WrapItem>
+    <WrapItem bg={'gray'}>
       <Center
         key={wine.id}
         width={{ base: '156px', lg: '256px'}}
         height={'333px'}
-        borderWidth={'1px'}
         flexDirection={'column'}
         padding={'10px'}
         justifyContent={'start'}
         bg={'white'}
-        shadow={'sm'}
+        shadow={'lg'}
       >
         <Image src={wine.image} alt={wine.name} height={'178.13px'}/>
         <Image
@@ -26,7 +26,7 @@ export default function CardWine(props: { wine: WinesType}) {
           ml={{ base: '-105px', lg: '170px' }}
           mt={{ base: '135px', lg: '125px' }}
         />
-        <Box>{wine.name}</Box>
+        <Text align={'center'}>{wine.name}</Text>
       </Center>
     </WrapItem>
   );
