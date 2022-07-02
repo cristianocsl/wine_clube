82b576e76aad56c6c96eca48b29693ab73664154
cristianocsl <cslcristiano@gmail.com>
2022-07-02 13:55:38 -0300

    styles: adiciona breakpoint de redimensionamento de margens e tamanhos de cards para atender a responsividade

--- components/CardWine.tsx
+++ components/CardWine.tsx
@@ -4,18 +4,20 @@ import { WinesType } from "../services/typing";
 
 export default function CardWine(props: { wine: WinesType}) {
   const { wine } = props;
-  
+
   return (
-    <WrapItem bg={'gray'}>
+    <WrapItem margin={'0px'}>
       <Center
         key={wine.id}
-        width={{ base: '156px', lg: '256px'}}
+        width={{ base: '140px', sm: '156px', lg: '256px'}}
         height={'333px'}
         flexDirection={'column'}
         padding={'10px'}
         justifyContent={'start'}
         bg={'white'}
         shadow={'lg'}
+        mr={{ base: '14px', smm: '18px'}}
+        ml={{ base: '14px', smm: '18px'}}
       >
         <Image src={wine.image} alt={wine.name} height={'178.13px'}/>
         <Image
