4a4ddea9038276b9558fd8e9c527f6166a1f6c26
cristianocsl <cslcristiano@gmail.com>
2022-07-02 00:25:42 -0300

    feat: adiciona cards

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Header from '../components/Header'
+import Wines from '../components/Wines'
 
 const Home: NextPage = () => {
   return (
@@ -14,6 +15,7 @@ const Home: NextPage = () => {
       </Head>
 
       <Header />
+      <Wines />
     </div>
   )
 }
