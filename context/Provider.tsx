82f4fde41ca6309eea0971c34bdc4dc2a7bd4c13
cristianocsl <cslcristiano@gmail.com>
2022-07-04 00:43:03 -0300

    feat: implementa função de filtro e estado filtrado de valores

--- context/Provider.tsx
+++ context/Provider.tsx
@@ -7,6 +7,7 @@ type Props = { children: ReactElement | ReactElement[] };
 
 const Provider = ({ children }: Props) => {
   const [wines, setWines] = useState<Array<WinesType>>([]);
+  const [filteredWines, setFilteredWines] = useState<Array<WinesType>>([]);
 
   useEffect(() => {
     const get = async () => {
@@ -18,29 +19,39 @@ const Provider = ({ children }: Props) => {
   }, []);
 
   function filterByPrice(value: number) {
-    return wines.filter((wine) => {
-      if (value === 1) {
-        return wine.priceMember <= 40;
+    console.log('value: ', value);
+      if (value === 6) {
+        setFilteredWines(wines);
+      }
+      else if (value === 1) {
+        const filtered = wines.filter((wine) => wine.priceMember <= 40);
+        
+        setFilteredWines(filtered);
       }
       else if (value === 2) {
-        return wine.priceMember <= 60;
+        const filtered = wines.filter((wine) => wine.priceMember > 40 && wine.priceMember <= 60);
+        setFilteredWines(filtered);
       }
       else if (value === 3) {
-        return wine.priceMember <= 200;
+        const filtered = wines.filter((wine) => wine.priceMember > 60 && wine.priceMember <= 200);
+        setFilteredWines(filtered);
       }
       else if (value === 4) {
-        return wine.priceMember <= 500;
+        const filtered = wines.filter((wine) => wine.priceMember > 200 && wine.priceMember <= 500);
+        setFilteredWines(filtered);
       }
       else if (value === 5) {
-        return wine.priceMember > 500;
+        const filtered = wines.filter((wine) => wine.priceMember > 500);
+        setFilteredWines(filtered);
       }
-    });
-  }
+    };
+  
 
   const context = {
     wines,
     setWines,
     filterByPrice,
+    filteredWines,
   };
 
   return (
