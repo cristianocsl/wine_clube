cb79d2c3935e2b7742533f402b23295ba3002c2d
cristianocsl <cslcristiano@gmail.com>
2022-07-03 22:03:26 -0300

    styles: ajusta posicionamento e cor de testox de radio buttons

---
+++ components/RadioGroup.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { Stack, Radio, RadioGroup, Text } from '@chakra-ui/react'
+
+export default function MyRadioGroup() {
+  return (
+    <RadioGroup bg={'background'}
+      width={{ base: '0px', lg: '550px'}}
+      display={{ base: 'none', lgg: 'block'}}
+      pl={'0px'}
+      color={'#1D1D1B'}
+      mt={'100px'}
+      ml={'50px'}
+    >
+      <Stack>
+        <Radio value='1'>Até R$40</Radio>
+        <Radio value='2'>R$40 A R$60</Radio>
+        <Radio value='3'>R$100 A R$200</Radio>
+        <Radio value='4'>R$200 A R$500</Radio>
+        <Radio value='5'>Acima de R$200</Radio>
+      </Stack>
+    </RadioGroup>
+  )
+}
+
+
