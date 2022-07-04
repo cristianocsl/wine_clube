04f78b8f661e95239afa6c33ee9d3f5d7e9bd379
cristianocsl <cslcristiano@gmail.com>
2022-07-03 22:30:37 -0300

    feat: adiciona a função de filtro por preço

--- context/Provider.tsx
+++ context/Provider.tsx
@@ -17,9 +17,30 @@ const Provider = ({ children }: Props) => {
     get();
   }, []);
 
+  function filterByPrice(value: number) {
+    return wines.filter((wine) => {
+      if (value === 1) {
+        return wine.priceMember <= 40;
+      }
+      else if (value === 2) {
+        return wine.priceMember <= 60;
+      }
+      else if (value === 3) {
+        return wine.priceMember <= 200;
+      }
+      else if (value === 4) {
+        return wine.priceMember <= 500;
+      }
+      else if (value === 5) {
+        return wine.priceMember > 500;
+      }
+    });
+  }
+
   const context = {
     wines,
     setWines,
+    filterByPrice,
   };
 
   return (
