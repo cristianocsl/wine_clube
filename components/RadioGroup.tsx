81e7df35a59c4f51e5964e79b0d519f73618301e
cristianocsl <cslcristiano@gmail.com>
2022-07-04 00:39:59 -0300

    feat: adiciona filtro em onChange de radio group

--- components/RadioGroup.tsx
+++ components/RadioGroup.tsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import { Stack, Radio, RadioGroup, Text } from '@chakra-ui/react'
 
 export default function MyRadioGroup(props: any) {
-  const { setRadioValue } = props;
+  const { filterByPrice } = props;
   return (
     <RadioGroup bg={'background'}
       width={{ base: '0px', lg: '550px'}}
@@ -11,14 +11,15 @@ export default function MyRadioGroup(props: any) {
       color={'#1D1D1B'}
       mt={'100px'}
       ml={'50px'}
-      onChange={setRadioValue}
+      onChange={ (value: string) => filterByPrice(+value) }
     >
       <Stack>
         <Radio value='1'>Até R$40</Radio>
         <Radio value='2'>R$40 A R$60</Radio>
         <Radio value='3'>R$100 A R$200</Radio>
         <Radio value='4'>R$200 A R$500</Radio>
-        <Radio value='5'>Acima de R$200</Radio>
+        <Radio value='5'>Acima de R$500</Radio>
+        <Radio value='6'>Todos</Radio>
       </Stack>
     </RadioGroup>
   )
